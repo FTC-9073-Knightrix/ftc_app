0a9dd24af38d4e421642f874d26c9f7aad1ebab7
Nickstreme <nicobravo1@hotmail.com>
2016-10-08 10:54:50 -0400

    Created the HardwareMap and Telemetry files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by nicolasbravo on 10/8/16.
+ * For use in the [Driver Controlled/Autonomous] Portion of the match
+ * [Description]
+ */
+
+public abstract class HardwareMap extends OpMode{
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telemetry.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.util.Hardware;
+
+/**
+ * Created by nicolasbravo on 10/8/16.
+ * For use in the [Driver Controlled/Autonomous] Portion of the match
+ * [Description]
+ */
+
+public abstract class Telemetry extends HardwareMap{
+
+
+}
