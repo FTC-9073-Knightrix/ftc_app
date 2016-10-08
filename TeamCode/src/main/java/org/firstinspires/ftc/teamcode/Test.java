a7de878d4468bf2572748e01b0387d0318106153
Nickstreme <nicobravo1@hotmail.com>
2016-10-08 11:31:30 -0400

    Made a program to test the motor by pressing 'A'

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+//adds the file to the FtcOpmodeRegister
+@TeleOp(name = "Test")
+
+/**
+ * Created by nicolasbravo on 10/8/16.
+ * For use in the [Driver Controlled/Autonomous] Portion of the match
+ * [Description]
+ */
+
+public abstract class Test extends Telemetry{
+
+    @Override
+    public void start(){
+
+        super.start();
+    }
+
+    //main loop
+    @Override
+    public void loop(){
+
+        if(gamepad1.a){
+
+            MoveMotor1(1);
+        }
+        else{
+
+            MoveMotor1(0);
+        }
+
+    }
+}
