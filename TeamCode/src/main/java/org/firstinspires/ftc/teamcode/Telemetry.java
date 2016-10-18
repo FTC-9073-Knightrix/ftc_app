1f3b72d2a979a55abddae0a1f006e690a0674929
Nickstreme <nicobravo1@hotmail.com>
2016-10-17 20:49:51 -0400

    Added comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telemetry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telemetry.java
@@ -6,14 +6,11 @@ package org.firstinspires.ftc.teamcode;
  */
 
 public abstract class Telemetry extends HardwareMap{
-
     public void UpdateTelemetry (){
         //telemetry data goes here
         telemetry.addLine(Double.toString(getRuntime()));
         telemetry.addLine("Motor 1 Speed: " + Motor1.getPower());
         telemetry.addLine("Encoder Position: " + Motor1.getCurrentPosition());
         telemetry.addLine("Motor Mode: " + Motor1.getMode());
-
-
     }
 }
