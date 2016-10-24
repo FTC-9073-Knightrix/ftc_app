aaecbf0e8b4475fc758fb379eae7606c3d3923fe
Nickstreme <nicobravo1@hotmail.com>
2016-10-24 19:58:36 -0400

    Made the program detect which color from the beacon is in front of the color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telemetry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telemetry.java
@@ -21,17 +21,27 @@ public abstract class Telemetry extends HardwareMap{
         telemetry.addLine("Ultrasonic: " + Range1.getDistance(DistanceUnit.CM) + " cm");
         telemetry.addLine("Optical: " + Range1.cmOptical() + " cm");
         telemetry.addLine("~Color Sensor~");
-        telemetry.addLine("RGB: " + (4 * Color1.red()) + ", " + (4 * Color1.green()) + ", " + (4 * Color1.blue()));
-        if(Color == 0){
-            telemetry.addLine("Beacon Color: Blue");
-        }
-        else if(Color == 1){
-            telemetry.addLine("Beacon Color: Red");
-        }
-        else if(Color == 2){
-            telemetry.addLine("Beacon Color: Other");
-        }
+        //telemetry.addLine("RGB: " + (4 * Color1.red()) + ", " + (4 * Color1.green()) + ", " + (4 * Color1.blue()));
+        //If 'Color1' is not null
+        if (Color1 != null){
+            //If there is more blue than red
+            if (Color1.blue() > Color1.red()){
+                telemetry.addLine("Beacon Color: Blue");
+            }
+            //Else if there is more red than blue
+            else if (Color1.red() > Color1.blue()){
+                telemetry.addLine("Beacon Color: Red");
+            }
+            //Else if they are the same value but not zero
+            else if (Color1.blue() == Color1.red() && Color1.blue() != 0 && Color1.red() != 0){
+                telemetry.addLine("Beacon Color: Both");
+            }
+            //Else
+            else{
+                telemetry.addLine("Beacon Color: Neither");
+            }
+        }/*
         telemetry.addLine("~Line Tracker~");
-        telemetry.addLine("Value: " + Voltage1);
+        telemetry.addLine("Value: " + Voltage1);*/
     }
 }
