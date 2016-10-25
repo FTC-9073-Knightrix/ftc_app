14636e2a6fc64dfc230347c709b33cc31946fd4d
Nickstreme <nicobravo1@hotmail.com>
2016-10-24 21:11:33 -0400

    Changed the way we programmed the right and left motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HDRIVE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HDRIVE.java
@@ -15,6 +15,7 @@ public class HDRIVE extends Telemetry{
     //Main loop
     @Override
     public void loop(){
+        /*
         //If gamepad 1's left stick is not equal to 0
         if (gamepad1.left_stick_y != 0){
             //Move the left motor by the value of gamepad 1's left stick
@@ -25,16 +26,25 @@ public class HDRIVE extends Telemetry{
             //Move the right motor by the value of gamepad 1's right stick
             MoveRightDrive(gamepad1.right_stick_y);
         }
+        */
+
         //If gamepad 1's left dpad is pressed
         if (gamepad1.dpad_left){
             //Move the middle motor left
             MoveMiddleDrive(-1);
         }
-        //If gamepad 1's right dpad is pressed
-        if (gamepad1.dpad_right){
+        //Else if gamepad 1's right dpad is pressed
+        else if (gamepad1.dpad_right){
             //Move the middle motor right
             MoveMiddleDrive(1);
         }
+        //Else if gamepad 1's left dpad and right dpad are not pressed
+        else if (!gamepad1.dpad_left && !gamepad1.dpad_right){
+            //Stop the middle motor
+            MoveMiddleDrive(0);
+        }
+        MoveLeftDrive(gamepad1.left_stick_y);
+        MoveRightDrive(gamepad1.right_stick_y);
 
         //Displays telemetry data
         UpdateTelemetry();
