91bbf79a2c7b4bb09e5d3d41c300a2445cdd2352
Nickstreme <nicobravo1@hotmail.com>
2016-10-24 20:21:52 -0400

    Created a program to run the motors used in the H Drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HDRIVE.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+//adds the file to the FtcOpmodeRegister
+@TeleOp(name = "H Drive", group = "H Drive Programs")
+
+/**
+ * Created by Nicolas Bravo & Vijay Rudraraju on 10/24/16.
+ * Testing
+ * Moves the motors on the H Drive
+ */
+
+public class HDRIVE extends Telemetry{
+    //Main loop
+    @Override
+    public void loop(){
+        //If gamepad 1's left stick is not equal to 0
+        if (gamepad1.left_stick_y != 0){
+            //Move the left motor by the value of gamepad 1's left stick
+            MoveLeftDrive(gamepad1.left_stick_y);
+        }
+        //If gamepad 1's right stick is not equal to 0
+        if (gamepad1.right_stick_y != 0){
+            //Move the right motor by the value of gamepad 1's right stick
+            MoveRightDrive(gamepad1.right_stick_y);
+        }
+        //If gamepad 1's left dpad is pressed
+        if (gamepad1.dpad_left){
+            //Move the middle motor left
+            MoveMiddleDrive(-1);
+        }
+        //If gamepad 1's right dpad is pressed
+        if (gamepad1.dpad_right){
+            //Move the middle motor right
+            MoveMiddleDrive(1);
+        }
+
+        //Displays telemetry data
+        UpdateTelemetry();
+    }
+}
+
