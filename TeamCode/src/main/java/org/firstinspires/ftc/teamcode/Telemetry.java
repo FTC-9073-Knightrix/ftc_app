5e3a09b3199240fae1f0dcb2cae4fd58d1960ba2
Nickstreme <nicobravo1@hotmail.com>
2016-10-29 12:24:04 -0400

    Added comments and the line tracker

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telemetry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Telemetry.java
@@ -13,41 +13,49 @@ public abstract class Telemetry extends HardwareMap{
     public void UpdateTelemetry (){
         //telemetry data goes here
         telemetry.addLine("Time: " + Double.toString(getRuntime()));
+        //Middle Wheel
         telemetry.addLine("~Middle Motor~");
-        telemetry.addLine("Speed: " + MiddleDrive.getPower() + "%");
+        telemetry.addLine("Speed: " + (MiddleDrive.getPower() * 100) + "%");
         telemetry.addLine("Encoder Position: " + MiddleDrive.getCurrentPosition());
-        telemetry.addLine("Mode: " + MiddleDrive.getMode());
+        //Left Wheel
         telemetry.addLine("~Left Motor~");
-        telemetry.addLine("Speed: " + LeftDrive.getPower() + "%");
-        telemetry.addLine("Mode: " + LeftDrive.getMode());
+        telemetry.addLine("Speed: " + (LeftDrive.getPower() * 100) + "%");
+        //Right Wheel
         telemetry.addLine("~Right Motor~");
-        telemetry.addLine("Speed: " + RightDrive.getPower() + "%");
-        telemetry.addLine("Mode: " + RightDrive.getMode());
+        telemetry.addLine("Speed: " + (RightDrive.getPower() * 100) + "%");
+        //Ball Pickup
+        telemetry.addLine("~Ball Pickup~");
+        telemetry.addLine("Speed: " + (PickupDrive.getPower() * 100) + "%");
+        //Range Sensor & Optical Sensor
         telemetry.addLine("~Range Sensor~");
         telemetry.addLine("Ultrasonic: " + Range1.getDistance(DistanceUnit.CM) + " cm");
-        telemetry.addLine("Optical: " + Range1.cmOptical() + " cm");
+        //Color Sensor
         telemetry.addLine("~Color Sensor~");
-        //telemetry.addLine("RGB: " + (4 * Color1.red()) + ", " + (4 * Color1.green()) + ", " + (4 * Color1.blue()));
         //If 'Color1' is not null
         if (Color1 != null){
             //If there is more blue than red
             if (Color1.blue() > Color1.red()){
+                //Displays the color of the beacon as blue
                 telemetry.addLine("Beacon Color: Blue");
             }
             //Else if there is more red than blue
             else if (Color1.red() > Color1.blue()){
+                //Displays the color of the beacon as red
                 telemetry.addLine("Beacon Color: Red");
             }
             //Else if they are the same value but not zero
             else if (Color1.blue() == Color1.red() && Color1.blue() != 0 && Color1.red() != 0){
+                //Displays the color of the beacons as both
                 telemetry.addLine("Beacon Color: Both");
             }
-            //Else
+            //Else (if none apply)
             else{
+                //Display the color of the beacon as neither
                 telemetry.addLine("Beacon Color: Neither");
             }
-        }/*
+        }
+        //Line Tracker
         telemetry.addLine("~Line Tracker~");
-        telemetry.addLine("Value: " + Voltage1);*/
+        telemetry.addLine("Value: " + Line1.getVoltage());
     }
 }
