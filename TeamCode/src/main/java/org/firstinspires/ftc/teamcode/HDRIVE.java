9294e743b7ce63194ce9a4ee15ba1ea5b6156b50
Nickstreme <nicobravo1@hotmail.com>
2016-10-29 10:53:28 -0400

    Changed the way the middle motor is run

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HDRIVE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HDRIVE.java
@@ -15,36 +15,9 @@ public class HDRIVE extends Telemetry{
     //Main loop
     @Override
     public void loop(){
-        /*
-        //If gamepad 1's left stick is not equal to 0
-        if (gamepad1.left_stick_y != 0){
-            //Move the left motor by the value of gamepad 1's left stick
-            MoveLeftDrive(gamepad1.left_stick_y);
-        }
-        //If gamepad 1's right stick is not equal to 0
-        if (gamepad1.right_stick_y != 0){
-            //Move the right motor by the value of gamepad 1's right stick
-            MoveRightDrive(gamepad1.right_stick_y);
-        }
-        */
-
-        //If gamepad 1's left dpad is pressed
-        if (gamepad1.dpad_left){
-            //Move the middle motor left
-            MoveMiddleDrive(-1);
-        }
-        //Else if gamepad 1's right dpad is pressed
-        else if (gamepad1.dpad_right){
-            //Move the middle motor right
-            MoveMiddleDrive(1);
-        }
-        //Else if gamepad 1's left dpad and right dpad are not pressed
-        else if (!gamepad1.dpad_left && !gamepad1.dpad_right){
-            //Stop the middle motor
-            MoveMiddleDrive(0);
-        }
-        MoveLeftDrive(gamepad1.left_stick_y);
-        MoveRightDrive(gamepad1.right_stick_y);
+        MoveMiddleDrive(gamepad1.right_trigger-gamepad1.left_trigger);
+        MoveLeftDrive(gamepad1.right_stick_y);
+        MoveRightDrive(gamepad1.left_stick_y);
 
         //Displays telemetry data
         UpdateTelemetry();
