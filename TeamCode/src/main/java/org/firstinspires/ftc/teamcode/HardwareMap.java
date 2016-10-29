ee12a8f7caa655d1f6843f6dbe32d897dc3c1d27
Nickstreme <nicobravo1@hotmail.com>
2016-10-29 13:57:35 -0400

    Changed the direction of the motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 
 /**
@@ -28,13 +29,13 @@ public abstract class HardwareMap extends OpMode{
     public void init(){
         //Motor1
         MiddleDrive = hardwareMap.dcMotor.get("M1");
-        MiddleDrive.setDirection(DcMotor.Direction.FORWARD);
+        MiddleDrive.setDirection(DcMotor.Direction.REVERSE);
         //LeftDrive
         LeftDrive = hardwareMap.dcMotor.get("M2");
-        LeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        LeftDrive.setDirection(DcMotor.Direction.REVERSE);
         //RightDrive
         RightDrive = hardwareMap.dcMotor.get("M3");
-        RightDrive.setDirection(DcMotor.Direction.REVERSE);
+        RightDrive.setDirection(DcMotor.Direction.FORWARD);
         //BallPickup
         PickupDrive = hardwareMap.dcMotor.get("M4");
         PickupDrive.setDirection(DcMotor.Direction.FORWARD);
